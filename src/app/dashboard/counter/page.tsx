e180fe7d10a7b93a6be9d273ac54bbf4d6bd48fe
andersonNB <andersonaugustonb@ufps.edu.co>
2025-02-02 12:49:27 -0500

    FEATURE: Dashboard tailwindCSS

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CounterPage() {
+	return (
+		<div className="bg-red-500">
+			<span className="text-black ">CounterPage</span>
+		</div>
+	);
+}
