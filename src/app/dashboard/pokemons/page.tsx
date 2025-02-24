8e4d9d73d039f9377bb6aff80c000ff8a8bcb277
andersonNB <andersonaugustonb@ufps.edu.co>
2025-02-24 09:14:20 -0500

    FEATURE: endpoint pokemons

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,17 +1,34 @@
+import {Pokemon, SimplePokemon} from "@/app/pokemons";
+import Image from "next/image";
 import React from "react";
 
-const getPokemons = async (limit = 20, offset = 0) => {
-	const data = await fetch(
+const getPokemons = async (
+	limit = 20,
+	offset = 0
+): Promise<SimplePokemon[]> => {
+	const data: Pokemon = await fetch(
 		"https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + offset
 	).then((res) => res.json());
 
-	return data;
+	const pokemons = data.results.map((pokemon) => ({
+		id: pokemon.url.split("/")[6],
+		name: pokemon.name,
+	}));
+
+	return pokemons;
 };
 
 const PokemonPage = async () => {
 	const pokemons = await getPokemons();
 
-	return <div className="w-2">{JSON.stringify(pokemons)}</div>;
+	return (
+		<div className="flex flex-col">
+			<div className="flex flex-wrap gap-10 items-center justify-center"></div>
+			{pokemons.map((pokemon, index) => (
+				<Image alt={pokemon?.name} src={pokemon.id} key={index} />
+			))}
+		</div>
+	);
 };
 
 export default PokemonPage;
---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,2 @@
+export type {Pokemon} from "./interfaces/pokemons-response";
+export type {SimplePokemon} from "./interfaces/simple-pokemon";
