1b712cd6a000afb3842525a4aa8089e46915dca8
andersonNB <andersonaugustonb@ufps.edu.co>
2025-04-14 21:33:31 -0500

    FEATURE: search by name pokemon

--- src/app/dashboard/pokemons/[name]/not-found.tsx
+++ src/app/dashboard/pokemons/[name]/not-found.tsx
@@ -7,11 +7,11 @@ export default function NotFound() {
 				404
 			</h1>
 			<div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
-				Page Not Found Global
+				Pokemon Not Found 😢
 			</div>
 
 			<Link
-				href="/dashboard/counter"
+				href="/dashboard/pokemons"
 				className="mt-5 relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
 			>
 				<span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0" />
--- src/app/dashboard/pokemons/[name]/page.tsx
+++ src/app/dashboard/pokemons/[name]/page.tsx
@@ -1,33 +1,43 @@
-"use client";
-import Image from "next/image";
-import {notFound} from "next/navigation";
-import React, {use, useEffect, useState} from "react";
 import {Pokemon} from "@/app/pokemons/interfaces/pokemon";
-import {FaArrowLeft} from "react-icons/fa";
+import Image from "next/image";
 import Link from "next/link";
+import {notFound} from "next/navigation";
+import React from "react";
+import {FaArrowLeft} from "react-icons/fa";
 
 interface Props {
-	params: Promise<{id: string}>;
+	params: Promise<{name: string}>;
 }
 
-const getPokemon = async (id: string): Promise<Pokemon> => {
-	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+export const metadata = {
+	title: "Nombre del pokemon",
+	description: "Descripcion del pokemon",
+};
+
+// generamos los params estaticos para la ruta dinamica, en tiempo de build
+export async function generateStaticParams() {
+	return [{name: "ditto"}, {name: "blastoise"}, {name: "metapod"}];
+}
+
+const getPokemon = async (name: string): Promise<Pokemon> => {
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
 	console.log(res);
 	if (!res.ok) notFound();
 	return res.json();
 };
 
-// export async function generateStaticParams() {
-// 	return [
-// 		{id: "1"},
-// 		{id: "2"},
-// 		{id: "3"},
-// 		{id: "4"},
-// 		{id: "5"},
-// 		{id: "6"},
-// 		{id: "7"},
-// 	];
-// }
+const InfoCard = ({
+	label,
+	children,
+}: {
+	label: string;
+	children: React.ReactNode;
+}) => (
+	<div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+		<p className="text-sm text-gray-600">{label}</p>
+		<div className="text-base font-medium text-navy-700 flex">{children}</div>
+	</div>
+);
 
 const SpritesSection = ({pokemon}: {pokemon: Pokemon}) => (
 	<>
@@ -69,48 +79,9 @@ const SpritesSection = ({pokemon}: {pokemon: Pokemon}) => (
 	</>
 );
 
-const InfoCard = ({
-	label,
-	children,
-}: {
-	label: string;
-	children: React.ReactNode;
-}) => (
-	<div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
-		<p className="text-sm text-gray-600">{label}</p>
-		<div className="text-base font-medium text-navy-700 flex">{children}</div>
-	</div>
-);
-
-export default function PokemonPage({params}: Props) {
-	const {id} = use(params);
-	const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-	const [isHovered, setIsHovered] = useState(false);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const data = await getPokemon(id);
-				setPokemon(data);
-			} catch (error) {
-				// Si la API lanza un error, el `notFound()` ya maneja el redireccionamiento
-				console.error(error);
-			} finally {
-				setLoading(false);
-			}
-		};
-		fetchData();
-	}, [id]);
-
-	// if (!pokemon) return <Error />; -> esto es un error manual que se puede lanzar si no encuentra el pokémon, la cuestion era el estado incial salia por un segundo y luego mostraba el pokemon
-	if (loading) {
-		return (
-			<div className="flex flex-col items-center justify-center h-screen w-full bg-[#1A2238] text-white">
-				Cargando...
-			</div>
-		);
-	}
+const DetailPokemon = async ({params}: Props) => {
+	const resolvedParams = await params;
+	const pokemon = await getPokemon(resolvedParams?.name);
 
 	return (
 		<div className="flex mt-5 flex-col items-center text-slate-800">
@@ -129,28 +100,14 @@ export default function PokemonPage({params}: Props) {
 					</div>
 
 					<div className="flex flex-col justify-center items-center">
-						<div
-							className="relative w-[150px] h-[150px]"
-							onMouseEnter={() => setIsHovered(true)}
-							onMouseLeave={() => setIsHovered(false)}
-						>
-							{isHovered ? (
-								<video
-									src="/videos/pokemon-demo.mp4"
-									autoPlay
-									loop
-									muted
-									className="w-[150px] h-[150px] object-contain opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-100"
-								/>
-							) : (
-								<Image
-									src={pokemon?.sprites.other?.dream_world.front_default ?? ""}
-									width={150}
-									height={150}
-									alt={`Imagen del pokemon ${pokemon?.name}`}
-									className="mb-5"
-								/>
-							)}
+						<div className="relative w-[150px] h-[150px]">
+							<Image
+								src={pokemon?.sprites.other?.dream_world.front_default ?? ""}
+								width={150}
+								height={150}
+								alt={`Imagen del pokemon ${pokemon?.name}`}
+								className="mb-5"
+							/>
 						</div>
 
 						<div className="flex flex-wrap">
@@ -181,4 +138,6 @@ export default function PokemonPage({params}: Props) {
 			</div>
 		</div>
 	);
-}
+};
+
+export default DetailPokemon;
