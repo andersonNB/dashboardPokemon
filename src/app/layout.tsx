3eb47d20bdba446e8f61ae8c247d387ab139344b
andersonNB <andersonaugustonb@ufps.edu.co>
2025-04-27 12:03:04 -0500

    FEATURE: CounterSlice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {Sidebar} from "./components";
+import {Providers} from "./store/Providers";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-orange-400 flex min-h-screen`}
 			>
-				<Sidebar />
-				{children}
+				<Providers>
+					<Sidebar />
+					{children}
+				</Providers>
 			</body>
 		</html>
 	);
