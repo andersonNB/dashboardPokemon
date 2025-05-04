11b754db03596051a06a187963af645daa414d3c
andersonNB <andersonaugustonb@ufps.edu.co>
2025-05-03 23:01:50 -0500

    no content - how works the hydratation problems

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,30 +1,36 @@
 "use client";
+import {SimplePokemon} from "@/app/pokemons";
 import {PokemonGrid} from "@/app/pokemons/components/PokemonGrid";
 import {useAppSelector} from "@/app/store";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {IoHeartOutline} from "react-icons/io5";
 
 const PokemonPage = () => {
 	const pokemonsStore = useAppSelector((state) => state.pokemons.favorites);
 
-	const favoritePokemons = Object.values(pokemonsStore);
-
-	// const [pokemons] = useState(favoritePokemons);
+	const [pokemons, setPokemons] = useState<SimplePokemon[]>([]);
 
 	useEffect(() => {
-		console.log("render");
-	}, []);
+		const favoritePokemons = Object.values(pokemonsStore);
+		setPokemons(favoritePokemons ?? []);
+	}, [pokemonsStore]);
+
+	function showContent() {
+		if (pokemons.length === 0) return <NoFavorites />;
+		if (pokemons.length > 0) {
+			return <PokemonGrid pokemons={pokemons} />;
+		}
+
+		return "Cargando...";
+	}
 
 	return (
 		<div className="flex flex-col gap-3 bg-yellow-400  ml-4  w-full">
 			<span className="text-xl font-semibold">
 				Pokémons Favoritos <small className="text-gray-500">Global state</small>{" "}
 			</span>
-			{favoritePokemons?.length ? (
-				<PokemonGrid pokemons={favoritePokemons} />
-			) : (
-				<NoFavorites />
-			)}
+			{/* {pokemons?.length ? <PokemonGrid pokemons={pokemons} /> : <NoFavorites />} */}
+			{showContent()}
 		</div>
 	);
 };
