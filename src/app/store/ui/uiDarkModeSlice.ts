8142b92c2fb95162ed6bd20e9dd0a64c05de041a
andersonNB <andersonaugustonb@ufps.edu.co>
2025-05-12 20:28:05 -0500

    no comments

--- src/app/store/ui/uiDarkModeSlice.ts
+++ src/app/store/ui/uiDarkModeSlice.ts
@@ -5,6 +5,7 @@ interface UIDarkMode {
 }
 
 //TODO: revisar por que se genera el error de hidratación cuando se usa el localstorage como incializador
+// revisar si es la forma de inicalizar el local storage aca o si en el provider funciona
 const getInitialStateLocalStorage = (): UIDarkMode => {
 	if (typeof localStorage === "undefined") return {darkMode: "light"};
 
