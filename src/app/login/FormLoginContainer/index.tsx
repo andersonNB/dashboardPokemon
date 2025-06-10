999657b32d85154b61cb595a179425688bb35359
andersonNB <andersonaugustonb@ufps.edu.co>
2025-06-09 20:13:14 -0500

    some desing to the form

--- src/app/login/FormLoginContainer/index.tsx
+++ src/app/login/FormLoginContainer/index.tsx
@@ -8,7 +8,7 @@ const FormLoginContainer = () => {
 				<span>por favor ingrese el correo y la contraseña</span>
 			</div>
 
-			<form className="flex">
+			<form className="flex flex-col justify-center items-center gap-2">
 				<input type="text" title="Correo electronico" />
 				<input type="text" title="Contraseña" />
 			</form>
