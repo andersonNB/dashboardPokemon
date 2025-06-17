d56d3a2735663738651e725b0c2b3ff4d9015634
andersonNB <andersonaugustonb@ufps.edu.co>
2025-06-16 23:25:30 -0500

    FEATURE: beta desing

--- src/app/login/FormLoginContainer/index.tsx
+++ src/app/login/FormLoginContainer/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const FormLoginContainer = () => {
 	return (
-		<div className="h-full bg-orange-200 ">
+		<div className="h-full bg-orange-200 w-full">
 			<div>
 				<span>Credenciales</span>
 				<span>por favor ingrese el correo y la contraseña</span>
 			</div>
 
-			<form className="flex flex-col justify-center items-center gap-2">
+			<form className="flex flex-col justify-center items-center gap-2 w-full">
 				<input type="text" title="Correo electronico" />
 				<input type="text" title="Contraseña" />
 				<button>Iniciar sesión</button>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,14 +6,11 @@ import FormLoginContainer from "./FormLoginContainer";
 const LoginPage = () => {
 	return (
 		<div className="flex justify-center items-center w-full h-screen">
-			<div className="flex items-center gap-2 bg-slate-500 rounded-md p-2">
-				<Image
-					src={login}
-					alt="imagen login"
-					height={600}
-					style={{borderRadius: 8}}
-				/>
-				<div className="h-full ">
+			<div className="flex items-center gap-2 bg-slate-500 rounded-md p-2 w- ">
+				<div className=" h-1/2 w-1/2 ">
+					<Image src={login} alt="imagen login" style={{borderRadius: 8}} />
+				</div>
+				<div className="h-full w-full bg-red-400">
 					<FormLoginContainer />
 				</div>
 			</div>
