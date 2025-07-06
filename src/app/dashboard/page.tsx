1b48adeaf96cc121036748f74d9172d45f72cf6f
andersonNB <andersonaugustonb@ufps.edu.co>
2025-07-05 20:07:52 -0500

    no comments

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function DashboardPage() {
-	return <div className="flex w-full min-h-screen bg-slate-700 ">qwerty</div>;
+	return <div className="flex w-full min-h-screen bg-slate-700 ">qwe</div>;
 }
